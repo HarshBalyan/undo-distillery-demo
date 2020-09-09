6e58041beb574087aa6c9dc342dc92ca875ea4dc
Harsh Balyan <harshbalyan@protonmail.com>
2020-09-10 04:23:36 +0530

    Allow server dev releases

--- config/dev.exs
+++ config/dev.exs
@@ -9,7 +9,8 @@ use Mix.Config
 config :demo, DemoWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
-  code_reloader: true,
+  server: true,
+  code_reloader: false,
   check_origin: false,
   watchers: [
     node: [
