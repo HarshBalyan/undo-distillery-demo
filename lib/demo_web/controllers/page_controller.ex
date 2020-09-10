4927674187af22a85755e8e04bf2157a3cbdd16d
Harsh Balyan <harshbalyan@protonmail.com>
2020-09-10 05:50:08 +0530

    Template correction

--- lib/demo_web/controllers/page_controller.ex
+++ lib/demo_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule DemoWeb.PageController do
   use DemoWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html", my_vars: %{})
+    my_vars = Application.get_env(:demo, :config_vars)
+    render(conn, "index.html", my_vars: my_vars)
   end
 end
